a983ed2c50ea6648507e89a3043c6e5c1c9e896e
ma-b <1753356+ma-b@users.noreply.github.com>
2025-01-31 01:13:46 +0100

    Check subgraphs

---
+++ Spindles/test/paper.jl
@@ -0,0 +1,17 @@
+@testset "Verify graphs from paper" begin
+    
+    B = readrational("../examples/s-25-5.txt", BigInt)
+    d = ones(Rational{BigInt}, size(B,1))
+    s = Spindle(B, d)
+
+    gfs = [f for f in facesofdim(s, 2) if isgood2face(s, f).good]
+    @test length(gfs) == 32
+
+    testfaces = [
+        [3,15,17], [3,9,15], 
+        [2,9,11], [2,9,13], [2,13,18], [9,13,22],
+        [2,8,18], [2,8,16], [2,16,22], [16,21,22],
+        [3,5,13], [3,13,23], [13,21,23]
+    ]
+    @test all(f in facesofdim(s, 2) for f in testfaces)
+end
