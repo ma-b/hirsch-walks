69be9aa806c8d65ecda7ab4827b1b1650008e58b
ma-b <1753356+ma-b@users.noreply.github.com>
2025-01-31 01:12:43 +0100

    Test cube

---
+++ Spindles/test/cube.jl
@@ -0,0 +1,16 @@
+@testset "Check prescribed apex" begin
+    # unit cube
+    A = [1 0 0; 0 1 0; 0 0 1; -1 0 0; 0 -1 0; 0 0 -1]
+    b = [1,1,1,1,1,1]
+    cube = Spindle(A, b)
+
+    success = true
+    for i=1:nvertices(cube)
+        try
+            apices(cube, i)
+        catch
+            success = false 
+        end
+    end
+    @test success
+end
