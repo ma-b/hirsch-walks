586d93b59b81739cde1820dbfa41a3cbedc07a86
Documenter.jl <documenter@juliadocs.github.io>
2025-02-04 22:15:11 +0000

    build based on ec2523e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Spindles.jl","page":"Home","title":"Spindles.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Test","category":"page"}]
+}
