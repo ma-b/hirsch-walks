6693cf7c67ed5ceffc6f5c2266386e23894e0471
ma-b <1753356+ma-b@users.noreply.github.com>
2025-02-10 15:49:32 +0100

    Add test for bugfix

--- Spindles/test/rep.jl
+++ Spindles/test/rep.jl
@@ -1,19 +1,18 @@
-using Polyhedra: hrep
+using Polyhedra: polyhedron, vrep, hrep
 
+@testset "Tests for constructors" begin
+    # TODO 1-dimensional
+    p = polyhedron(vrep([1 0; 0 1]))
+    Spindle(p)
+end
+
+#=
 @testset "Tests for representations" begin
     A, b, _ = readineq("../examples/s-25-5.txt", BigInt)
     s1 = Spindle(A, b)
-    s2 = Spindle(s1.P)
-    s3 = Spindle(s2.A, s2.b)
+    s2 = Spindle(s1.p)
+    s3 = Spindle(hrep(s2.p).A, hrep(s2.p).b)
 
-    @test hrep(s1.P).A == s1.A
-    @test s1.A == s2.A == s3.A  # TODO
-    @test s1.b == s2.b == s3.b  # TODO
-    @test s1.P == Spindle(s1.A,s1.b).P  # fails
-    @test s1.P == s2.P
-    @test s2.P == s3.P # fails
-    #@test s == s2  # TODO
-
-    #using Polyhedra
-    #@test nvertices(s1) == npoints(s1.P)
+    #@test nvertices(s1) == npoints(s1.p)
 end
+=#
