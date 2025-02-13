9b6574806004570e066681b012cf027139ac3dcd
ma-b <1753356+ma-b@users.noreply.github.com>
2025-02-13 20:54:13 +0100

    Rename example spindles

--- Spindles/examples/hirsch-i.jl
+++ Spindles/examples/hirsch-i.jl
@@ -43,30 +43,30 @@ labels
 #     the assignment to the rows of `A` is different.
 
 # The apices are
-s48 = Spindle(A, b)
-collect(vertices(s48))[apices(s48)]
+s = Spindle(A, b)
+collect(vertices(s))[apices(s)]
 
-# and the distance between them in the graph of `s48` is indeed 6:
-dist(s48, apices(s48)...)
+# and the distance between them in the graph of `s` is indeed 6:
+dist(s, apices(s)...)
 
 # Note that both apices are highly degenerate:
 using Graphs: degree
-degree(graph(s48), apices(s48))
+degree(graph(s), apices(s))
 
 #src ==========================
 # ## Inspecting faces
 
 # The original motivation for developing *Spindles.jl* was the search for special two-dimensional faces
-# (or *2-faces* for short) of the spindle `s48` and similar spindles. 
+# (or *2-faces* for short) of the spindle `s` and similar spindles. 
 # To explain what we mean by "special", let us consider the following three facets:
 face = [29, 37, 41]
 labels[face]
 
-# They indeed define a 2-face of `s48`:
-face in facesofdim(s48, 2)
+# They indeed define a 2-face of `s`:
+face in facesofdim(s, 2)
 
 # We may use the function [`plot2face`](@ref) provided by *Spindles.jl* to plot the graph of this face.
-plot2face(s48, face; facetlabels=labels, showdist=true, figsize=(400,400))
+plot2face(s, face; facetlabels=labels, showdist=true, figsize=(400,400))
 #md nothing #hide
 
 #md # This produces the following output:
@@ -74,12 +74,12 @@ plot2face(s48, face; facetlabels=labels, showdist=true, figsize=(400,400))
 #md # ![](../assets/s-48-5.svg)
 
 # As one would expect from a 2-face, the graph is a cycle. Each vertex is labeled by its index 
-# (on the first line) and the distance to each of the two apices of `s48` (on the second line).
+# (on the first line) and the distance to each of the two apices of `s` (on the second line).
 # For example, `1 | 5` in the label of vertex `25` means that `25` is one edge step away from 
-# the first apex `apices(s48)[1]` and five steps away from `apices(s48)[2]`.
+# the first apex `apices(s)[1]` and five steps away from `apices(s)[2]`.
 # Likewise, the label `0 | 6` for vertex `1` means that `1` is at distance 0 from `apices(s)[1]`.
 # This should not be surprising given that `1` *is* the index of the first apex:
-apices(s48)
+apices(s)
 
 # Edges are labeled by their incident facets, where the three facets that define the face are omitted.
 
@@ -100,15 +100,15 @@ apices(s48)
 # (the subset consisting of `56`, `155`, `156`, and `80`) is disjoint from the
 # first one, and each vertex in $V_2$ is "close" to second apex . Here, "close" means that
 # if we pick two arbitrary vertices, one from each subset, then the sum of their distances
-# to the respectively closest apex is at most 3, which is 2 less than the dimension of `s48`.
+# to the respectively closest apex is at most 3, which is 2 less than the dimension of `s`.
 # Moreover, each vertex in $V_2$ is at least 3 edge steps away from $V_1$ on the face.
 
 # Let us call 2-faces with this property *good*. Good 2-faces play an important role for
-# analyzing `s48` in the setting of the so-called *circuit diameter conjecture*, a relaxation
+# analyzing `s` in the setting of the so-called *circuit diameter conjecture*, a relaxation
 # of the Hirsch conjecture. *Spindles.jl* provides a function [`isgood2face`](@ref) that tests
 # a face for being good.
 
-isgood2face(s48, face)
+isgood2face(s, face)
 
 # The result is wrapped in a bespoke data type called
 # [`FaceState`](@ref Spindles.FaceState). The field `good` indicates whether or not
@@ -116,13 +116,13 @@ isgood2face(s48, face)
 # of being good are stored in the field `vsets` (see also the documentation on the
 # [`FaceState`](@ref Spindles.FaceState) type):
 
-fstate = isgood2face(s48, face)
+fstate = isgood2face(s, face)
 fstate.good, fstate.vsets
 
 # Let us plot it again, but this time not as a graph but as a two-dimensional projection.
 # This is achieved by setting the option `usecoordinates` to `true`:
 plot2face(
-	s48, face; facetlabels=labels, 
+	s, face; facetlabels=labels, 
 	showdist=true, usecoordinates=true, directed_edges=fstate.edges, figsize=(400,400)
 )
 #md nothing #hide
@@ -131,26 +131,27 @@ plot2face(
 
 #md # ![](../assets/s-48-5_geom.svg)
 
-# We may also enumerate all good 2-faces of `s48` as follows.
-for f in sort(facesofdim(s48, 2))
-    if isgood2face(s48, f).good
+# We may also enumerate all good 2-faces of `s` as follows.
+for f in sort(facesofdim(s, 2))
+    if isgood2face(s, f).good
         println(join(labels[f], " "))
     end
 end
 
 # In particular, for each good 2-face, there must exist paths from both apices to
 # some vertex on the face of total length at most 3. 
-# Interestingly, all 2-faces of `s48` that satisfy this weaker condition are good:
-for f in sort(facesofdim(s48, 2))
+# Interestingly, all 2-faces of `s` that satisfy this weaker condition are good:
+for f in sort(facesofdim(s, 2))
 	min_total_length = sum(
 		minimum(
-			dist(s48, a, v) for v in incidentvertices(s48, f)
-		) for a in apices(s48)
+			dist(s, a, v) for v in incidentvertices(s, f)
+		) for a in apices(s)
 	)
 	if min_total_length <= 3
-		println(join(labels[f], " "), "\t", isgood2face(s48, f).good)
+		println(join(labels[f], " "), "\t", isgood2face(s, f).good)
 	end
 end
 
 # If you would like to explore one of the actual counterexamples to the Hirsch conjecture
-# that was built from a spindle like `s48`, please read on [here]("Spindles and the Hirsch conjecture II").
+# that was built from a spindle like `s`, please read on 
+#src [here]("Spindles and the Hirsch conjecture II").
--- Spindles/examples/hirsch-ii.jl
+++ Spindles/examples/hirsch-ii.jl
@@ -18,20 +18,20 @@
 # To begin, let us enumerate the good 2-faces of the 5-dimensional spindle.
 
 using Spindles
-A_5, b_5, _ = readineq("s-25-5.txt", BigInt)
-s_5 = Spindle(A_5, b_5)
+A, b, _ = readineq("s-25-5.txt", BigInt)
+s = Spindle(A, b)
 
 # !!! note
-#     We created the spindle `s_5` from rational data with numerators and denominators of type `BigInt` 
+#     We created the spindle `s` from rational data with numerators and denominators of type `BigInt` 
 #     (this is the second argument passed to `readineq`). Choosing `Int` here (as in [Part I]() 
 #     of this tutorial) would have produced an integer overflow error. See also the section on 
 #     [arbitrary-precision arithmetic](https://docs.julialang.org/en/v1/manual/integers-and-floating-point-numbers/#Arbitrary-Precision-Arithmetic)
 #     in the Julia language documentation.
 
-# The following code finds all good 2-faces of `s_5`.
+# The following code finds all good 2-faces of `s`.
 goodfaces = []
-for f in sort(facesofdim(s_5, 2))
-    fstate = isgood2face(s_5, f)
+for f in sort(facesofdim(s, 2))
+    fstate = isgood2face(s, f)
     if fstate.good
         push!(goodfaces, fstate)
     end
@@ -44,7 +44,7 @@ using Plots
 plot_arr = []
 for fstate in goodfaces
     push!(plot_arr, 
-        plot2face(s_5, fstate.facets; showdist=true, directed_edges=fstate.edges)
+        plot2face(s, fstate.facets; showdist=true, directed_edges=fstate.edges)
     )
 end
 
@@ -67,23 +67,23 @@ plot(plot_arr..., layout=(nrows, ncols), size=(1000, nrows*300));
 print(read("s-25.txt", String))
 
 # You may notice that the coefficients in the first couple of columns are very similar to
-# those of `A_5`, the coefficient matrix of the 5-dimensional "base" spindle `s_5`. This
-# similarity is no coincidence: the matrix encoded in the file above is derived from `A_5`
+# those of `A`, the coefficient matrix of the 5-dimensional "base" spindle `s`. This
+# similarity is no coincidence: the matrix encoded in the file above is derived from `A`
 # in a highly structured way.
-# Each row has a "counterpart" in `A_5`. For convenience, each row above is already
+# Each row has a "counterpart" in `A`. For convenience, each row above is already
 # labeled by the index of its "counterpart". For example, the labels of the first three rows
 # (the numbers in the first column) are
 [3, 7, 2]
 
-# and the corresponding rows of `A_5` are
-A_5[[3, 7, 2],:]
+# and the corresponding rows of `A` are
+A[[3, 7, 2],:]
 
 # If you inspect those row labels closely, you will notice a pattern. Two labels appear multiple times,
-# namely  `11` and `25`. The corresponding rows of `A_5` have been "replicated" a number of times.
+# namely  `11` and `25`. The corresponding rows of `A` have been "replicated" a number of times.
 # Each time, a new nonzero entry is added to the right whose order of magnitude is much larger than
 # that of all other coefficients. At the polyhedral level, this "replication" of rows is reflected by
 # an operation called *wedging*, which plays a crucial role in Santos' construction of a Hirsch
-# counterexample from spindles like `s_5`.
+# counterexample from spindles like `s`.
 
 # The structure that we just observed will be extremely useful in "guessing" good 2-faces of the
 # 20-dimensional spindle encoded in the file above. Before we examine its faces, let us first
@@ -92,57 +92,57 @@ A_5[[3, 7, 2],:]
 #src ==========================
 # ### Building the spindle
 
-A_20, b_20, labels = readineq("s-25.txt", BigInt)
-s_20 = Spindle(A_20, b_20)
+A20, b20, labels = readineq("s-25.txt", BigInt)
+s20 = Spindle(A20, b20)
 
-collect(vertices(s_20))[apices(s_20)]
+collect(vertices(s20))[apices(s20)]
 
-# Note that `s_20` is simple:
+# Note that `s20` is simple:
 using Graphs: degree
-all(degree(graph(s_20)) .== dim(s_20))
+all(degree(graph(s20)) .== dim(s20))
 
 # Its most important property, however, is the length of a shortest path between the apices:
-dist(s_20, apices(s_20)...)
+dist(s20, apices(s20)...)
 
 # The Hirsch conjecture would imply that there must be a strictly shorter path, namely of length 20.
-# Therefore, `s_20` is a counterexample to the Hirsch conjecture.
+# Therefore, `s20` is a counterexample to the Hirsch conjecture.
 
 # You may have noticed that computations in dimension 20 take longer than they did in dimension 5.
-# Calling `graph` or `dist` for the first time on `s_20` (or any `Spindles` function that needs
-# the graph, in fact) triggers the computation of the entire graph of `s_20`. Luckily, we don't
-# have to enumerate all 2-faces of `s_20` to identify good ones. 
+# Calling `graph` or `dist` for the first time on `s20` (or any `Spindles` function that needs
+# the graph, in fact) triggers the computation of the entire graph of `s20`. Luckily, we don't
+# have to enumerate all 2-faces of `s20` to identify good ones. 
 # Instead, we may take advantage of the structure in its coefficient matrix to "guess" good 2-faces.
 
 #src ==========================
 # ## Guessing good 2-faces
 
 # For example, here is one of the good 2-faces of `s` again:
-face_5 = [2,8,9]
-isgood2face(s_5, face_5).good
+face = [2,8,9]
+isgood2face(s, face).good
 
-# The corresponding facets of `s_20` are
-face_20 = [i for (i,label) in enumerate(labels) if parse(Int, label) in face_5]
+# The corresponding facets of `s20` are
+face20 = [i for (i,label) in enumerate(labels) if parse(Int, label) in face]
 
 # However, three facets do not make a 2-face in dimension 20 yet. We need another 15 facet-defining
-# inequalities from the description of `s_20`. With some geometric intuition of what the wedging
+# inequalities from the description of `s20`. With some geometric intuition of what the wedging
 # operation does, we propose the following rule of thumb: To get up to 18 facets, pick facets from
 # those two blocks of "replications" labeled `11` and `25`. Specifically, from each block, pick all
 # facets but one. Let us calculate the number of facets in each block:
 sum(labels .== "11"), sum(labels .== "25")
 
 # So, in total, our proposed rule of thumb would indeed give us the desired number of $9+8-2=15$ facets.
-# Let us "validate" this rule on `face_5`.
+# Let us "validate" this rule on `face`.
 blocks = [findall(labels .== ref) for ref in ["11", "25"]]
-face_20 = [face_20; blocks[1][2:end]; blocks[2][2:end]]
-println(join(unique(labels[face_20]), " "))
-isgood2face(s_20, face_20).good
+face20 = [face20; blocks[1][2:end]; blocks[2][2:end]]
+println(join(unique(labels[face20]), " "))
+isgood2face(s20, face20).good
 
-# Great! By omitting the first facet from each block, we immediately found a good 2-face of `s_20`.
-# Let us plot this face and the original face `face_5` side by side.
+# Great! By omitting the first facet from each block, we immediately found a good 2-face of `s20`.
+# Let us plot this face and the original face `face` side by side.
 
 plot(
-    plot2face(s_5, face_5; usecoordinates=true),
-    plot2face(s_20, face_20; usecoordinates=true, facetlabels=labels),
+    plot2face(s, face; usecoordinates=true),
+    plot2face(s20, face20; usecoordinates=true, facetlabels=labels),
     layout=grid(1,2), size=(800,300)
 )
 #md nothing #hide
@@ -154,12 +154,12 @@ plot(
 
 # Not only do their projections look very similar, they are also combinatorially almost identical:
 
-edges_5  = isgood2face(s_5, face_5).edges
-edges_20 = isgood2face(s_20, face_20).edges
+edges  = isgood2face(s, face).edges
+edges20 = isgood2face(s20, face20).edges
 
 plot(
-    plot2face(s_5, face_5; directed_edges=edges_5, showdist=true),
-    plot2face(s_20, face_20; directed_edges=edges_20, showdist=true, facetlabels=labels),
+    plot2face(s, face; directed_edges=edges, showdist=true),
+    plot2face(s20, face20; directed_edges=edges20, showdist=true, facetlabels=labels),
     layout=grid(1,2), size=(800,400)
 )
 #md nothing #hide
@@ -170,11 +170,11 @@ plot(
 #md # ![](../assets/s-25_compare_geom.svg)
 
 # The figure on the right is the graph of the 2-face in dimension 20, with facets labeled by 
-# which facets of the 5-dimensional spindle `s_5` they correspond to. Combinatorially,
+# which facets of the 5-dimensional spindle `s` they correspond to. Combinatorially,
 # the only change is an additional edge (the one defined by `4`). Most importantly, however,
 # the property of being good is preserved.
 
 # Let us take this one step further and find such a good 2-face in dimension 20 for each of
-# the good 2-faces of `s_5`.
+# the good 2-faces of `s`.
 
 #src TODO
