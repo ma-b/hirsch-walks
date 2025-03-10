7808aad050f96d905363795799496ec7707f2b95
ma-b <1753356+ma-b@users.noreply.github.com>
2025-03-10 14:36:25 +0100

    Begin cyclic order with least index

--- Spindles/src/goodfaces.jl
+++ Spindles/src/goodfaces.jl
@@ -57,9 +57,10 @@ end
 
 # check whether `g` is a cycle: if yes, list the vertices of `g` in cyclic order; if not, return nothing.
 # the arguments are the return values of Graphs.induced_subgraph
+# least index first in order
 function cyclicorder(g::Graphs.SimpleGraph, vmap::Vector{Int})
     # pick an arbitrary starting vertex and traverse the graph g depth-first
-    start = first(Graphs.vertices(g))
+    start = minimum(Graphs.vertices(g))  # least vertex index first
     cyclic = [start]
 
     v = start
