0a790c9f8c394c51e6a25012f62d34aa67711fe0
ma-b <1753356+ma-b@users.noreply.github.com>
2025-06-06 22:59:01 +0200

    Merge and update tests

--- Spindles/test/redundancy.jl
+++ Spindles/test/redundancy.jl
@@ -2,35 +2,41 @@
     @testset "Cube" begin
         # minimal description of the 3D unit cube
         A = [1 0 0; 0 1 0; 0 0 1; -1 0 0; 0 -1 0; 0 0 -1]
-        b = [1,1,1,1,1,1]
+        b = [1, 1, 1, 1, 1, 1]
         # add redundant inequalities, some of which are NOT facet-defining
         B = [A; 1 1 1; 1 1 0; 2 0 0]
         d = [b; 3; 2; 2]
 
-        cube = Polytope(B, d)
-        @test nvertices(cube) == 8
+        p = Polytope(B, d)
+        @test nvertices(p) == 8
+        @test p == Polytope(A, b)
 
-        isfacet(i::Int) = codim(cube, i) == 1
+        isfacet(i::Int) = codim(p, i) == 1
 
-        @test all( isfacet.([1:6; 9])) # facets
+        @test  all(isfacet.([1:6; 9])) # facets
         @test !any(isfacet.([7,8]))    # non-facets
 
-        # test codim of lower-dim faces that are already defined by a subset of their tight inequalities
-        # (see also issue_codim.jl)
-        for i in ineqindices(cube)
-            @test dim(cube, i) + codim(cube, i) == dim(cube)
+        # test codim of lower-dim faces that are already defined by a subset of their 
+        # tight inequalities (see also issue_codim.jl)
+        for i in ineqindices(p)
+            @test dim(p, i) + codim(p, i) == dim(p)
         end
 
-        for v=1:nvertices(cube)
-            apx = apices(cube, v)
+        for v=1:nvertices(p)
+            apx = apices(p, v)
             # each vertex has an antipodal one that we may take as a second apex:
             @test apx !== nothing
             # antipodal pair means they must sum to 0:
-            @test sum(collect(vertices(cube))[apx]) == [0,0,0]
+            @test sum(collect(vertices(p))[apx]) == [0,0,0]
         end
 
+        # try to find apices without eliminating redundancy first; 
+        # this should fail for at least one vertex
+        @test any(isnothing, apices(p, v; checkredund=false) for v=1:nvertices(p))
+
         # remove redundant rows and check whether polytope is the same
-        # TODO
+        # (we know that the polytope is full-dimensional)
+        @test p == Polytope(facets(p)...)
     end
 
     @testset "Introduce redundancy to s48" begin
@@ -44,7 +50,4 @@
         @test [1,0,0,0,0] in apx
         @test [-1,0,0,0,0] in apx
     end
-
-    # test facets and impliciteqs disjoint
-
 end
--- Spindles/test/repr.jl
+++ Spindles/test/repr.jl
@@ -1,14 +1,16 @@
-using Polyhedra: hrep
+using Polyhedra: polyhedron, hrep
 
-@testset "Tests for constructors" begin
-    @testset "Explicit and implicit equation" begin
+@testset "Tests for constructors and representations" begin
+    @testset "Explicit vs implicit equations I" begin
+        # line segment
         p = Polytope([1 0; 0 1])
         
-        # same, but with an implicit equation
+        # same but with an implicit equation
         A = [-1 0; 1 1; 0 -1; -1 -1]
         b = [0, 1, 0, -1]
         q = Polytope(A, b)
 
+        # both must be spindles
         @test apices(p) !== nothing
         @test apices(q) !== nothing
 
@@ -18,6 +20,23 @@ using Polyhedra: hrep
         @test reduce(.&, q.inc) == (codim.(q, ineqindices(q)) .== 0)
     end
 
+    @testset "Explicit vs implicit equations II" begin
+        A = [-1 0; 1 0; 0 -1; 0 1]
+        b = [0, 1, 0, 0]
+
+        # two implicit equations
+        p = Polytope(A, b)
+        # make one of them an explicit equality constraint
+        q = Polytope(polyhedron(hrep(A, b, BitSet([3]))))
+
+        @test p == q
+
+        @test inequalities(p) == (A, b)
+        @test inequalities(q) == (A[[1,2,4],:], b[[1,2,4]])
+        @test affinehull(p) == affinehull(q)
+        @test facets(p) == facets(q)
+    end
+
     @testset "Preserve indices" begin
         A = [1 0; 0 1; -1 0; 0 -1]
         b = [1, 1, 1, 1]
@@ -25,6 +44,7 @@ using Polyhedra: hrep
 
         @test hrep(s.poly).A == A
         @test hrep(s.poly).b == b
+        @test inequalities(s) == (A, b)
     end
 
     @testset "Non-spindle" begin
@@ -34,8 +54,21 @@ using Polyhedra: hrep
 
     @testset "Redundant and duplicate point" begin
         p = Polytope([1 0; 0 1; 0 1; 1//2 1//2]);
-
-        #collect(vertices(p))
         @test nvertices(p) == 2
+    end
+
+    @testset "Argument errors" begin
+        A = [1 0; 0 1; -1 0; 0 -1]
+        b = [1, 1, 1, 1]
+
+        @test try
+            Polytope(A, b[2:end])  # wrong dimensions
+            false
+        catch
+            true
+        end
+
+        # vertex index out of bounds
+        @test try apices(Polytope(A, b), 10); false catch; true end
     end
 end
