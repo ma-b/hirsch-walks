b01d37c3301697bb860e6800c344262fef0f7951
ma-b <1753356+ma-b@users.noreply.github.com>
2025-06-06 23:31:23 +0200

    Add tests from permutahedra tutorial

---
+++ Spindles/test/linesegments.jl
@@ -0,0 +1,42 @@
+# tests taken from examples/permutahedra.jl
+using Graphs: edges, src, dst
+
+@testset "Tests for polytope generators" begin
+
+    @testset "Hypercubes" begin
+        n = 4
+        lineseg = Polytope([[0], [1]])
+        p = reduce(*, repeat([lineseg], n))
+
+        @test cube(n) == 2p - ones(n)
+        @test p == 1//2 * (cube(n) + ones(Int, n))
+
+        @test cube(n) == -cube(n)  # standard hypercube is symmetric around 0
+
+        @test lineseg == simplex(1)
+        @test p == reduce(*, repeat([simplex(1)], n))
+    end
+
+    @testset "Permutahedra" begin
+        n = 4
+        directions = [Int.(1:n .== i) - Int.(1:n .== j) for i=1:n for j=i+1:n]
+        p = collect(1:n) + sum(d -> Polytope([zeros(Int, n), d]), directions)
+
+        @test dim(p) == n-1
+        @test nvertices(p) == factorial(n)
+        @test p == permutahedron(n)
+
+        # the edge directions of `p` must be precisely the vectors in `directions`
+        edge_directions = unique([
+            reduce(-, collect(vertices(p))[[dst(e), src(e)]])
+            for e in edges(graph(p))
+        ])
+        @test sort(directions) == sort(edge_directions)
+
+        # as projections of hypercubes
+        T = hcat(directions...)
+        m = length(directions)
+        c = (cube(m) + ones(Int, m)) // 2  # 0/1 hypercube in dimension m
+        @test p == collect(1:n) + map(x -> T * x, c)
+    end
+end
