89cbf2937b8278a9efd7791ede744864175ded6d
ma-b <1753356+ma-b@users.noreply.github.com>
2025-06-07 13:08:31 +0200

    Add test for io

---
+++ Spindles/test/io.jl
@@ -0,0 +1,14 @@
+# taken from doctests
+@testset "Tests for I/O" begin
+    A = [1 0; 0 1; -1 0; 0 -1]
+    b = [1, 1, 1, 1]
+    labels = ["α", "β", "γ", "δ"]
+
+    fname = "iotest.txt"
+    writeineq(fname, A, b, labels; comments=["A nice polytope"])
+    
+    @test read(fname, String) == "# A nice polytope\nα  1  -1   0\nβ  1   0  -1\nγ  1   1   0\nδ  1   0   1"
+    @test readineq(fname, Int) == A, b, labels
+    
+    rm(fname)
+end
