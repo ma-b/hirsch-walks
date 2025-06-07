ca2c34688053ac6770b780535c3bbdb29c75749f
ma-b <1753356+ma-b@users.noreply.github.com>
2025-06-07 13:42:49 +0200

    Update io test

--- Spindles/test/io.jl
+++ Spindles/test/io.jl
@@ -4,11 +4,21 @@
     b = [1, 1, 1, 1]
     labels = ["α", "β", "γ", "δ"]
 
-    fname = "iotest.txt"
+    # generate random file name
+    fname = "test-" * string(abs(rand(Int)) % 100000000) * ".txt"
+
+    # user labels
     writeineq(fname, A, b, labels; comments=["A nice polytope"])
-    
     @test read(fname, String) == "# A nice polytope\nα  1  -1   0\nβ  1   0  -1\nγ  1   1   0\nδ  1   0   1"
     @test readineq(fname, Int) == (A, b, labels)
-    
+
+    # default labels
+    writeineq(fname, A, b)
+    @test readineq(fname, Int) == (A, b, string.(axes(A,1)))
+
+    # no labels
+    writeineq(fname, A, b; labels=false)
+    @test readineq(fname, Int; labels=false) == (A, b, nothing)
+
     rm(fname)
 end
